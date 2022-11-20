da0417a1f49a1a0e9ba2579d16bf5d2a0c85b87d
Mohammed <md.rejaul.karim@outlook.com>
2022-11-21 10:16:40 +1100

    fixed the user_data code to install httpd

--- ec2.tf
+++ ec2.tf
@@ -9,18 +9,15 @@ resource "aws_instance" "ec2_instance" {
   }
   subnet_id = aws_subnet.displayr-subnet-1.id
   vpc_security_group_ids = [ "${aws_security_group.security_group.id}" ]
-  user_data = file("nginx-install.sh")
-/*
-  user_data = <<-EOF
-    #!/bin/bash -xe
-    exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
+	user_data = file("httpd-install.sh")  
+  /*
+    user_data     = <<-EOF
+    #!/bin/bash
     sudo yum update -y
-    sudo amazon-linux-extras install nginx1 -y
-    sudo su -c "/bin/echo 'Hello DisplayR' >/usr/share/nginx/html/index.html"
-    instance_ip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
-    sudo su -c "echo $instance_ip >>/usr/share/nginx/html/index.html"
-    sudo systemctl start nginx
-    sudo systemctl enable  nginx
+    sudo yum install httpd -y
+    sudo systemctl enable httpd
+    sudo systemctl start httpd
+    echo "<html><body><div>Welcome to StackSimplify ! AWS Infra created using Terraform</div></body></html>" > /var/www/html/index.html
     EOF
-    */
+  */
     }
