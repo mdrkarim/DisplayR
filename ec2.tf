ba205908554dc8424fe86e3dd815f9c08e82b936
Mohammed <md.rejaul.karim@outlook.com>
2022-11-20 22:00:01 +1100

    added file function to read from nginx-install.sh

--- ec2.tf
+++ ec2.tf
@@ -9,15 +9,18 @@ resource "aws_instance" "ec2_instance" {
   }
   subnet_id = aws_subnet.displayr-subnet-1.id
   vpc_security_group_ids = [ "${aws_security_group.security_group.id}" ]
+  user_data = file("nginx-install.sh")
+/*
   user_data = <<-EOF
     #!/bin/bash -xe
     exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
     sudo yum update -y
     sudo amazon-linux-extras install nginx1 -y
-    sudo su -c "/bin/echo 'My Site: DevopsRoles.com' >/usr/share/nginx/html/index.html"
+    sudo su -c "/bin/echo 'Hello DisplayR' >/usr/share/nginx/html/index.html"
     instance_ip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
     sudo su -c "echo $instance_ip >>/usr/share/nginx/html/index.html"
     sudo systemctl start nginx
     sudo systemctl enable  nginx
     EOF
+    */
     }
